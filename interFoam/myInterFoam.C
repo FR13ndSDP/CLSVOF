82e457d9d9798bc47d43f60ed2f2e80ab380d28c
Fr13ndSDP <mylab_octave@163.com>
2020-04-21 17:48:52 +0800

    改造interFoam, 奇怪的结果，dambreak不收敛”

--- interFoam/myInterFoam.C
+++ interFoam/myInterFoam.C
@@ -38,7 +38,6 @@ Description
 \*---------------------------------------------------------------------------*/
 
 #include "fvCFD.H"
-#include "dynamicFvMesh.H"
 #include "CMULES.H"
 #include "EulerDdtScheme.H"
 #include "localEulerDdtScheme.H"
@@ -57,10 +56,7 @@ int main(int argc, char *argv[])
 {
     argList::addNote
     (
-        "Solver for two incompressible, isothermal immiscible fluids"
-        " using VOF phase-fraction based interface capturing.\n"
-        "With optional mesh motion and mesh topology changes including"
-        " adaptive re-meshing."
+        "瞎搞的interFoam"
     );
 
     #include "postProcess.H"
@@ -68,28 +64,41 @@ int main(int argc, char *argv[])
     #include "addCheckCaseOptions.H"
     #include "setRootCaseLists.H"
     #include "createTime.H"
-    #include "createDynamicFvMesh.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    #include "createTimeControls.H"
     #include "initContinuityErrs.H"
-    #include "createDyMControls.H"
     #include "createFields.H"
     #include "createAlphaFluxes.H"
-    #include "initCorrectPhi.H"
-    #include "createUfIfPresent.H"
+    #include "mycorrectPhi.H"
 
     turbulence->validate();
 
     if (!LTS)
     {
+        #include "readTimeControls.H"
         #include "CourantNo.H"
         #include "setInitialDeltaT.H"
     }
+
+    #include "mappingPsi.H"
+    #include "solveLSFunction.H"
+    #include "calcNewCurvature.H"
+
+    //NOTE: 验证质量守恒
+    autoPtr<OFstream> massFilePtr;
+
+    scalar totalMass = 1;
+    scalar totalMass0 = 1;
+    const scalarField& V = mesh.V();
+    totalMass0 = gSum(rho*V);
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
     Info<< "\nStarting time loop\n" << endl;
 
     while (runTime.run())
     {
-        #include "readDyMControls.H"
+        #include "readTimeControls.H"
 
         if (LTS)
         {
@@ -106,48 +115,11 @@ int main(int argc, char *argv[])
 
         Info<< "Time = " << runTime.timeName() << nl << endl;
 
+        H.storeOldTime();
+
         // --- Pressure-velocity PIMPLE corrector loop
         while (pimple.loop())
         {
-            if (pimple.firstIter() || moveMeshOuterCorrectors)
-            {
-                mesh.update();
-
-                if (mesh.changing())
-                {
-                    // Do not apply previous time-step mesh compression flux
-                    // if the mesh topology changed
-                    if (mesh.topoChanging())
-                    {
-                        talphaPhi1Corr0.clear();
-                    }
-
-                    gh = (g & mesh.C()) - ghRef;
-                    ghf = (g & mesh.Cf()) - ghRef;
-
-                    MRF.update();
-
-                    if (correctPhi)
-                    {
-                        // Calculate absolute flux
-                        // from the mapped surface velocity
-                        phi = mesh.Sf() & Uf();
-
-                        #include "mycorrectPhi.H"
-
-                        // Make the flux relative to the mesh motion
-                        fvc::makeRelative(phi, U);
-
-                        mixture.correct();
-                    }
-
-                    if (checkMeshCourantNo)
-                    {
-                        #include "meshCourantNo.H"
-                    }
-                }
-            }
-
             #include "alphaControls.H"
             #include "alphaEqnSubCycle.H"
 
@@ -157,6 +129,11 @@ int main(int argc, char *argv[])
             {
                 continue;
             }
+
+            #include "mappingPsi.H"
+            #include "solveLSFunction.H"
+            #include "calcNewCurvature.H"
+            #include "updateFlux.H"
 
             #include "UEqn.H"
 
@@ -172,7 +149,19 @@ int main(int argc, char *argv[])
             }
         }
 
+        // reInitialise the alpha equation 
+        if (runTime.outputTime())
+        {
+            Info<<"Overwriting alpha" << nl << endl;
+            alpha1 = H;
+            volScalarField& alpha10 = const_cast<volScalarField&>(alpha1.oldTime());
+            alpha10 = H.oldTime();
+            //const_cast<volScalarField&>(alpha1.storeOldTime()()) = H.oldTime();
+        }
+
         runTime.write();
+
+        #include "writeMass.H"
 
         runTime.printExecutionTime(Info);
     }
